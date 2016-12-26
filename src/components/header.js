431da1022a8ed5288815c856191fb72e2b3b5f90
Dave Harms <daveharmswebdev@gmail.com>
2016-12-26 13:45:34 -0600

    initial commit, created header, displays on simulator

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// import library fro making Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// make a Component
+const Header = () => <Text>Albums!</Text>;
+
+// make component available to other apps
+export default Header;
